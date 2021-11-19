ff9c62c5dd366497dd9453bfd85b220c8e825997
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2021-11-19 13:09:11 +0300

    added jquery for toogle

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    // first
+    $("#design").click(function(){
+        $("#text1").toggle();
+    });
+
+});
+$(document).ready(function(){
+   //    second
+   $("#developement").click(function(){
+      
+    $("#text2").toggle();
+    });
+});
+$(document).ready(function(){
+       // third
+       $("#management").click(function(){
+        $("#text3").toggle();
+    });
+ });
