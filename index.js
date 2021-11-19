cb82e4289abe6c6394b8e84c7db5fab26a975027
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2021-11-19 16:52:11 +0300

    addedjavascript functions

--- index.js
+++ index.js
@@ -18,3 +18,32 @@ $(document).ready(function(){
         $("#text3").toggle();
     });
  });
+
+ const validation = () =>{
+     var name = document.getElementById("submit").value.trim();
+     var username = document.getElementById("username").value.trim();
+     var usermail = document.getElementById("usermail").value.trim();
+     var message = document.getElementById("message").value.trim();
+     if(name == "" || username == "" || usermail == "" || message == ""){
+         alert("please fill all fields");
+         return false;
+     }else{
+        alert("Thank you"+username + "for contacting us");
+        var name = document.getElementById("submit").value="";
+        var username = document.getElementById("username").value="";
+        var usermail = document.getElementById("usermail").value="";
+        var message = document.getElementById("message").value="";
+         return false;
+     }
+ }
+ $(document).ready(function(){
+     $(".work").hover(function(){
+       $(this).css("color","white");
+       $(".bg_change").css("background-color","rgba(0,0,0,0.5)");
+       $(this).css("border","2px solid #fff");
+     },function(){
+         $(this).css("color","transparent");
+         $(this).css("border","none");
+         $(".bg_change").css("background-color","inherit");
+     });
+ });
